84c63f88bdf334400877ad34cafb6f7baf030594
Bruno <brunomartins1030@gmail.com>
2025-12-05 11:24:47 +0000

    Initial commit

---
+++ dynamic-bank-webapp/src/main/java/com/dynamicbank/webapp/DynamicBankWebappApplication.java
@@ -0,0 +1,13 @@
+package com.dynamicbank.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicBankWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicBankWebappApplication.class, args);
+	}
+
+}
