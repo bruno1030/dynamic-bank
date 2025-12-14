dbbc12f1c94cb35dae9fd10fd496d5d7b3b10f70
Bruno <brunomartins1030@gmail.com>
2025-12-14 21:18:06 +0000

    Initial commit

---
+++ dynamic-bank-account-service/src/main/java/com/dynamicbank/account/DynamicBankAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dynamicbank.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicBankAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicBankAccountServiceApplication.class, args);
+	}
+
+}
---
+++ dynamic-bank-account-service/src/test/java/com/dynamicbank/account/DynamicBankAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamicbank.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicBankAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
