e67101dcceefb0b708a6699c7484e29ebf5be68e
Bruno <brunomartins1030@gmail.com>
2025-12-15 22:34:25 +0000

    add AccountController

---
+++ dynamic-bank-account-service/src/main/java/com/dynamicbank/account/controller/AccountController.java
@@ -0,0 +1,16 @@
+package com.dynamicbank.account.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    public String createAccount(){
+        return null;
+    }
+
+    public String login(){
+        return null;
+    }
+
+}
